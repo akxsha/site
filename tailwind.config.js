04d5baf6eb6784dee76f5de597b2b31adb209ede
Mark Miro <contact@markmiro.com>
2024-08-28 15:26:09 -0700

    be yellow

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,8 +38,10 @@ export default {
       yellow: {
         50: '#fefce8',
         100: '#fef9c3',
+        200: '#fef08a',
         400: '#facc15',
         500: '#eab308',
+        600: '#ca8a04',
       },
       orange: {
         100: '#ffedd5',
